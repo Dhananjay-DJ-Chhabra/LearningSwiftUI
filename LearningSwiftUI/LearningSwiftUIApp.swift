c99fe63eeea163e39b58d0ae5e6f0cc4c6afb86a
Dhananjay Chhabra <dhananjay.chhabra@patientbond.com>
2023-07-01 17:41:44 +0530

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Dhananjay on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
